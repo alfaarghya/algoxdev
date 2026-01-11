6b09fb9f9e4c0671af85d6618706d2a1789beeab
alfaarghya <alfaarghya.dev@gmail.com>
2026-01-11 22:33:04 +0530

    init: ALGOxDEV

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ALGOxDEV | Algorithm Visualizer",
+  description: "Visualize Data Structure and Algorithms",
+  keywords: ["visualizer", "algorithm", "data structures"],
+  authors: [{ name: "ALGOxDEV" }],
+  openGraph: {
+    title: "ALGOxDEV | Algorithm Visualizer",
+    description: "Visualize Data Structure and Algorithms",
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
